7cc23483c6c4a6415da82bbe351a5ebb655556d1
chents <xxx@mail.com>
2020-12-29 18:32:03 +0800

    防止快速点击

--- WebApi/day6/js/index.js
+++ WebApi/day6/js/index.js
@@ -1,105 +1,112 @@
-window.addEventListener('DOMContentLoaded',function(){
-	
+window.addEventListener('DOMContentLoaded', function() {
+
 	var arrowl = document.querySelector('.arrow-l');
 	var arrowr = document.querySelector('.arrow-r');
 	var focus = document.querySelector('.focus');
-	var ol  = document.querySelector('ol');
+	var ol = document.querySelector('ol');
 	var ul = focus.querySelector('ul');
-	
+
 	var focusWidth = focus.offsetWidth;
 	//大盒子 鼠标经过
-	focus.addEventListener('mouseenter',function(){
-		
+	focus.addEventListener('mouseenter', function() {
+
 		arrowl.style.display = 'block';
 		arrowr.style.display = 'block';
 	})
-	
-	focus.addEventListener('mouseleave',function(){
-		
+
+	focus.addEventListener('mouseleave', function() {
+
 		arrowl.style.display = 'none';
 		arrowr.style.display = 'none';
-		
+
 	})
 	//总个数
 	var count = ul.children.length;
 	for (var i = 0; i < count; i++) {
 		// count[i].
 		var li = document.createElement('li');
-		li.setAttribute('index',i);
-		li.addEventListener('click',function(){
+		li.setAttribute('index', i);
+		li.addEventListener('click', function() {
 			// console.log('圆点-'+i);
-			
+
 		})
-		
+
 		ol.appendChild(li);
 	}
 	//默认圆点
-	ol.children[0].className='current';
-	
+	ol.children[0].className = 'current';
+
 	//克隆轮播图最后一张，添加到后面
 	var first = ul.children[0].cloneNode(true);
 	ul.appendChild(first);
 
-	arrowl.addEventListener('click',function(){
-		// 位移图片
-		if(num==0){
-			num= ul.children.length-1;
-			// console.log("left="+ul.style.left);
-			ul.style.left = -num*focusWidth+'px';
-			
+	arrowl.addEventListener('click', function() {
+
+		if (flag) {
+			flag = false;
+			// 位移图片
+			if (num == 0) {
+				num = ul.children.length - 1;
+				// console.log("left="+ul.style.left);
+				ul.style.left = -num * focusWidth + 'px';
+
+			}
+			num--;
+
+			animation();
+
+			circle--;
+
+			if (circle < 0) {
+				circle = ol.children.length - 1;
+			}
+			circleChange();
 		}
-		num--;
-		 
-		animation();
-		
-		circle--;
-		
-		if(circle<0){
-			circle = ol.children.length-1;
-		}
-		for (var i = 0; i < ol.children.length; i++) {
-			ol.children[i].className = '';
-		}
-		ol.children[circle].className ='current';
-		
-		
-		
 	})
-	var num=0;
-	var circle=0;
-	arrowr.addEventListener('click',function(){
-		
-		if(num==ul.children.length-1){
-			ul.style.left = 0+"px";
-			num=0;
+	var num = 0;
+	var circle = 0;
+	arrowr.addEventListener('click', function() {
+		if (flag) {
+			flag = false;
+			if (num == ul.children.length - 1) {
+				ul.style.left = 0 + "px";
+				num = 0;
+			}
+			// 位移图片
+			num++;
+			console.log('width=' + focusWidth);
+
+			animation();
+
+			//排他思想处理小圆点
+			circle++;
+			if (circle == ol.children.length) {
+				circle = 0;
+			}
+			circleChange();
 		}
-		// 位移图片
-		num++;
-		console.log('width='+focusWidth);
-		
-		animation();
-		
-		//排他思想处理小圆点
-		circle++;
-		if(circle==ol.children.length){
-			circle=0;
-		}
-		for (var i = 0; i < ol.children.length; i++) {
-			ol.children[i].className = '';
-		}
-		ol.children[circle].className ='current';
-		
+
+
 	})
-	
-	
-	function animation(){
-		
-		
-		animate(ul,- num * focusWidth,function(){
-			console.log('anim ok!')
+
+	var flag = true;
+
+	function animation() {
+
+		animate(ul, -num * focusWidth, function() {
+			flag = true;
 		});
 	}
-	
-	
-	
+
+	/**
+	 * 小圆点 
+	 */
+	function circleChange() {
+		for (var i = 0; i < ol.children.length; i++) {
+			ol.children[i].className = '';
+		}
+		ol.children[circle].className = 'current';
+	}
+
+
 })
