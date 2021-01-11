dfa7c4df8c55d3dc755130382552d7c2a3d941c4
chents <xxx@mail.com>
2021-01-11 22:15:19 +0800

    test tab切换

--- JS高级/day1/tab切换/js/tab.js
+++ JS高级/day1/tab切换/js/tab.js
@@ -1,100 +1,101 @@
 var that;
 class Tab {
-    constructor(id) {
-        // 获取元素
-        that = this;
-        this.main = document.querySelector(id);
-        this.add = this.main.querySelector('.tabadd');
-        // li的父元素
-        this.ul = this.main.querySelector('.fisrstnav ul:first-child');
-        // section 父元素
-        this.fsection = this.main.querySelector('.tabscon');
-        this.init();
-    }
-    init() {
-            this.updateNode();
-            // init 初始化操作让相关的元素绑定事件
-            this.add.onclick = this.addTab;
-            for (var i = 0; i < this.lis.length; i++) {
-                this.lis[i].index = i;
-                this.lis[i].onclick = this.toggleTab;
-                this.remove[i].onclick = this.removeTab;
-                this.spans[i].ondblclick = this.editTab;
-                this.sections[i].ondblclick = this.editTab;
+	constructor(id) {
+		// 获取元素 
+		that = this;
+		this.main = document.querySelector(id);
 
-            }
-        }
-        // 因为我们动态添加元素 需要从新获取对应的元素
-    updateNode() {
-            this.lis = this.main.querySelectorAll('li');
-            this.sections = this.main.querySelectorAll('section');
-            this.remove = this.main.querySelectorAll('.icon-guanbi');
-            this.spans = this.main.querySelectorAll('.fisrstnav li span:first-child');
-        }
-        // 1. 切换功能
-    toggleTab() {
-            // console.log(this.index);
-            that.clearClass();
-            this.className = 'liactive';
-            that.sections[this.index].className = 'conactive';
-        }
-        // 清除所有li 和section 的类
-    clearClass() {
-            for (var i = 0; i < this.lis.length; i++) {
-                this.lis[i].className = '';
-                this.sections[i].className = '';
-            }
-        }
-        // 2. 添加功能
-    addTab() {
-            that.clearClass();
-            // (1) 创建li元素和section元素 
-            var random = Math.random();
-            var li = '<li class="liactive"><span>新选项卡</span><span class="iconfont icon-guanbi"></span></li>';
-            var section = '<section class="conactive">测试 ' + random + '</section>';
-            // (2) 把这两个元素追加到对应的父元素里面
-            that.ul.insertAdjacentHTML('beforeend', li);
-            that.fsection.insertAdjacentHTML('beforeend', section);
-            that.init();
-        }
-        // 3. 删除功能
-    removeTab(e) {
-            e.stopPropagation(); // 阻止冒泡 防止触发li 的切换点击事件
-            var index = this.parentNode.index;
-            console.log(index);
-            // 根据索引号删除对应的li 和section   remove()方法可以直接删除指定的元素
-            that.lis[index].remove();
-            that.sections[index].remove();
-            that.init();
-            // 当我们删除的不是选中状态的li 的时候,原来的选中状态li保持不变
-            if (document.querySelector('.liactive')) return;
-            // 当我们删除了选中状态的这个li 的时候, 让它的前一个li 处于选定状态
-            index--;
-            // 手动调用我们的点击事件  不需要鼠标触发
-            that.lis[index] && that.lis[index].click();
-        }
-        // 4. 修改功能
-    editTab() {
-        var str = this.innerHTML;
-        // 双击禁止选定文字
-        window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty();
-        // alert(11);
-        this.innerHTML = '<input type="text" />';
-        var input = this.children[0];
-        input.value = str;
-        input.select(); // 文本框里面的文字处于选定状态
-        // 当我们离开文本框就把文本框里面的值给span 
-        input.onblur = function() {
-            this.parentNode.innerHTML = this.value;
-        };
-        // 按下回车也可以把文本框里面的值给span
-        input.onkeyup = function(e) {
-            if (e.keyCode === 13) {
-                // 手动调用表单失去焦点事件  不需要鼠标离开操作
-                this.blur();
-            }
-        }
-    }
+		this.add = this.main.querySelector('.tabadd');
+		this.ul = this.main.querySelector('.firstnav ul:first-child')
+		this.fsection = this.main.querySelector('.tabscon');
+
+		this.init();
+	}
+	//刷新父布局
+	updateNode() {
+		this.lis = this.main.querySelectorAll('li');
+		this.sections = this.main.querySelectorAll('section');
+		this.remove = this.main.querySelectorAll('.icon-guanbi');
+		this.spans = this.main.querySelectorAll('.firstnav li span:first-child');
+	}
+
+	init() {
+		this.updateNode();
+		for (var i = 0; i < this.lis.length; i++) {
+			//索引
+			this.lis[i].index = i;
+			this.lis[i].onclick = this.toggleTab;
+			this.remove[i].onclick = this.removeTab;
+			this.spans[i].ondblclick = this.editTab;
+		}
+		this.add.onclick = this.addTab;
+	}
+	//切换开关
+	toggleTab() {
+		console.log();
+		that.clearCss();
+		this.className = 'liactive';
+		that.sections[this.index].className = 'conactive';
+
+	}
+	clearCss() {
+		//清除tab和内容的状态
+		for (var i = 0; i < this.lis.length; i++) {
+			this.lis[i].className = '';
+			this.sections[i].className = '';
+		}
+	}
+
+	addTab() {
+
+		that.clearCss();
+		var random = Math.random();
+		//add li
+		var li = '<li class="liactive"><span>new Tab</span><span class="iconfont icon-guanbi"></span></li>';
+		that.ul.insertAdjacentHTML('beforeend', li);
+		// add section
+		var section = '<section class="conactive">' + random + '</section>';
+		that.fsection.insertAdjacentHTML('beforeend', section);
+
+		that.init();
+	}
+
+	removeTab(e) {
+		e.stopPropagation(); //阻止冒泡，防止触发li的点击事件
+		var index = this.parentNode.index;
+		console.log(index);
+		that.ul.children[index].remove();
+		that.sections[index].remove();
+			
+		that.init();
+		//如果是最后一个，返回
+		if(index==0){
+			return;
+		}
+		//关闭的选项之前一个为选中状态
+		that.lis[index-1].click();
+	}
+
+	editTab() {
+		window.getSelection? window.getSelection().removeAllRanges():document.selection.empty();
+		// console.log('hh');
+		var str = this.innerHTML;
+		this.innerHTML = '<input type= "text" value ="'+str+'"/>';
+		
+		var input = this.children[0];
+		input.select();//处于选定状态
+		input.onblur = function(){
+			//重新赋值
+			input.parentNode.innerHTML = this.value;
+		}
+		//实现点击回车，确认内容
+		input.onkeyup = function(e){
+			if(e.keyCode===13){
+				this.blur();
+			}
+		}
+	}
+
 
 }
 new Tab('#tab');
