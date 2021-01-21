771f040b73b7d5170995bbd410b3e471094774dc
chents <xxx@mail.com>
2021-01-21 20:41:40 +0800

    node.js

---
+++ Node.js/a.js
@@ -0,0 +1,6 @@
+let version = 1.0;
+
+const sayhi = name => 'hello '+name;
+
+exports.version = version;
+exports.sayhi = sayhi;
---
+++ Node.js/b.js
@@ -0,0 +1,5 @@
+let a = require('./a.js');
+
+let hi = a.sayhi('lili');
+console.log(hi);
+console.log(a.version);
